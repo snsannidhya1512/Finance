4ffeb6035418e4b7d8ad1182bc24ecf5a0d74ac7
sannidhya <snsannidhya1512@gmail.com>
2024-07-02 08:05:11 +0530

    hono.js setup

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,15 @@
-import {
-  clerkMiddleware,
-  createRouteMatcher
-} from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher([
-  '/', 
-]);
+const isProtectedRoute = createRouteMatcher(["/"]);
 
 export default clerkMiddleware((auth, request) => {
   if (isProtectedRoute(request)) {
     auth().protect();
   }
-  NextResponse.next()
- 
+  NextResponse.next();
 });
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
-
-
