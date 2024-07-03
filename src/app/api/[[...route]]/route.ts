7226a44293caf0a4f9f2fa5fd3e891b5340554f9
sannidhya <snsannidhya1512@gmail.com>
2024-07-03 22:35:55 +0530

    create accounts GET API

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,15 +1,21 @@
-import { z } from "zod";
-import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import authors from "./authors";
-import books from "./books";
+import accounts from "./accounts";
+import { HTTPException } from "hono/http-exception";
 
 export const runtime = "edge";
 const app = new Hono().basePath("/api");
 
-app.route("/authors", authors);
-app.route("/books", books);
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return err.getResponse();
+  }
+  return c.json({ error: "Internal Error" }, 500);
+});
+
+const routes = app.route("/accounts", accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
+
+export type AppType = typeof routes;
