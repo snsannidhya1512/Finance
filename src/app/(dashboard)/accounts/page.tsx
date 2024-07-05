e2d58ad60432640d09ab7fe5a6f112ebd7b3d58f
sannidhya <snsannidhya1512@gmail.com>
2024-07-05 12:10:12 +0530

    Accounts bulk delete with confirmation

--- src/app/(dashboard)/accounts/page.tsx
+++ src/app/(dashboard)/accounts/page.tsx
@@ -1,29 +1,37 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Plus } from "lucide-react";
+import { Loader2, Plus } from "lucide-react";
 import { useNewAccount } from "../../../../features/accounts/hooks/use-new-account";
-import { Payment, columns } from "./columns";
+import { columns } from "./columns";
 import { DataTable } from "@/components/data-table";
+import { useGetAccounts } from "../../../../features/accounts/api/use-get-accounts";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useBulkDeleteAccounts } from "../../../../features/accounts/api/use-bulk-delete";
 
 const AccountsPage = () => {
   const newAccount = useNewAccount();
+  const deleteAccounts = useBulkDeleteAccounts();
+  const accountsQuery = useGetAccounts();
+  const accounts = accountsQuery.data || [];
+  const isDisabled = accountsQuery.isLoading || deleteAccounts.isPending;
 
-  const data: Payment[] = [
-    {
-      id: "72ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-
-    {
-      id: "72ed53f",
-      amount: 400,
-      status: "success",
-      email: "a@example.com",
-    },
-  ];
+  if (accountsQuery.isLoading) {
+    return (
+      <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+        <Card className="border-none drop-shadow-sm">
+          <CardHeader>
+            <Skeleton className="h08 w-48 " />
+          </CardHeader>
+          <CardContent>
+            <div className="h-[500px w-full flex items-center justify-center">
+              <Loader2 className="text-slate-300 size-6 animate-spin" />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
 
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -39,9 +47,12 @@ const AccountsPage = () => {
           <DataTable
             filterKey="email"
             columns={columns}
-            data={data}
-            onDelete={() => {}}
-            disabled={false}
+            data={accounts}
+            onDelete={(row) => {
+              const ids = row.map((r) => r.original.id);
+              deleteAccounts.mutate({ ids });
+            }}
+            disabled={isDisabled}
           />
         </CardContent>
       </Card>
--- src/components/data-table.tsx
+++ src/components/data-table.tsx
@@ -22,6 +22,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { useConfirm } from "../../hooks/use-confirm";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Trash } from "lucide-react";
@@ -41,6 +42,10 @@ export function DataTable<TData, TValue>({
   onDelete,
   disabled,
 }: DataTableProps<TData, TValue>) {
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you Sure",
+    "You are about to perform bulk delete"
+  );
   const [sorting, setSorting] = React.useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
     []
@@ -65,6 +70,7 @@ export function DataTable<TData, TValue>({
 
   return (
     <div>
+      <ConfirmDialog />
       <div className="flex items-center py-4">
         <Input
           placeholder={`Filter ${filterKey}...`}
@@ -80,6 +86,14 @@ export function DataTable<TData, TValue>({
             size="sm"
             variant="outline"
             className="ml-auto font-normal text-xs"
+            onClick={async () => {
+              const ok = await confirm();
+
+              if (ok) {
+                onDelete(table.getFilteredSelectedRowModel().rows);
+                table.resetRowSelection();
+              }
+            }}
           >
             <Trash className="size-4 mr-2" />
             Delete ({table.getFilteredSelectedRowModel().rows.length})
