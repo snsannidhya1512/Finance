e8b6ec0d8b45595d8bf6d1d20c5b348f1ae339d2
sannidhya <snsannidhya1512@gmail.com>
2024-07-05 14:13:36 +0530

    Edit and delete single account with confirmation

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -3,13 +3,16 @@
 import { useEffect, useState } from "react";
 import { NewAccountSheet } from "../features/accounts/components/new-account-sheet";
 import { useMountedState } from "react-use";
+import { EditAccountSheet } from "../features/accounts/components/edit-account-sheet";
 
 export const SheetProvider = () => {
   const isMounted = useMountedState();
+  if (!isMounted) return null;
 
   return (
     <>
       <NewAccountSheet />
+      <EditAccountSheet />
     </>
   );
 };
